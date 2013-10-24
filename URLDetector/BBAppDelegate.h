fedd343e702b552e405e8e84be62e97732784865
beanandbean <wangsw.a@gmail.com>
2013-10-24 17:59:57 +0800

    Initial Commit

---
+++ URLDetector/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  URLDetector
+//
+//  Created by wangsw on 10/24/13.
+//  Copyright (c) 2013 beanandbean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ URLDetector/BBViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBViewController.h
+//  URLDetector
+//
+//  Created by wangsw on 10/24/13.
+//  Copyright (c) 2013 beanandbean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBViewController : UIViewController
+
+@end
